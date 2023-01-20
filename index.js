d08964154a3310030ce80bdfaf53fe8cf063b513
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-19 23:09:54 -0400

    Utilización de rutas

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import router from './routes/index.js';
 
 //? Crear el servidor
 const app = express();
+
+//? Habilitando las rutas
+app.use('/', router);
 
 app.listen(5000);
 console.log('Funcionando en el puerto 5000 🔥🔥')
